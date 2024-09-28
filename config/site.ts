05dfc25f8947f63d6b2d35acecfe752bef380559
Dustin <135719141+DustinTurska@users.noreply.github.com>
2024-09-27 23:04:02 -0500

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "OnChain Opinion Rewards",
+  description: "Get rewarded for your opninions OnChain!",
   navItems: [
     {
       label: "Home",
